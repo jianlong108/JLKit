84fae0d39a7a05b267634f8f430d86938a9497af
wangjianlong <wangjianlong@xiaomi.com>
2017-06-08 19:47:57 +0800

    增加 菜单view

---
+++ JLKitDemo/JLKitDemo/JLMenuView.h
@@ -0,0 +1,59 @@
+//
+//  JLMEenu.h
+//  JLMenuDemo
+//
+//  Created by 王建龙 on 15/11/28.
+//  Copyright © 2015年 AngelLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef NS_ENUM(NSInteger, MenuViewType) {
+    MenuViewTypeTableView = 1,          //表格样式
+    MenuViewTypeCollectionView = 2,     //方格样式
+    MenuViewTypeCustomView = 3          //自定义内容
+};
+typedef NS_ENUM(NSInteger, PointDirection) {
+    PointDirectionLeft = 1,          //箭头居左
+    PointDirectionMiddle = 2,        //箭头居中
+    PointDirectionRight = 3          //箭头居右
+};
+typedef NS_ENUM(NSInteger, PointAppearDirection) {
+    PointAppearDirectionTop    = 1,             //箭头出现在顶部
+    PointAppearDirectionLeft   = 2,             //箭头出现在左部
+    PointAppearDirectionBottom = 3,             //箭头出现在底部
+    PointAppearDirectionRight  = 4              //箭头出现在右部
+};
+
+typedef void(^Dismiss)(void);
+@class JLMenuView;
+@protocol JLMenuDataSource <NSObject>
+
+@required
+- (NSUInteger)numberOfSubmenusInCustomMenu:(JLMenuView *)menuView;
+- (NSArray<NSString *> *)titleForSubmenuInCustomMenu:(JLMenuView *)menuView;
+- (CGFloat)menuViewWidth:(JLMenuView *)menuView;
+@optional
+- (NSArray<UIImage *> *)imageForSubmenuInCustomMenu:(JLMenuView *)menuView;
+- (MenuViewType)menuViewType:(JLMenuView *)menuView;
+- (PointAppearDirection)menuViewPointAppearanceDirection:(JLMenuView *)menuView;
+- (PointDirection)menuViewDirection:(JLMenuView *)menuView;
+- (UIColor *)menuViewContentColor:(JLMenuView *)menuView;
+- (UIColor *)menuViewTitleColor:(JLMenuView *)menuView;
+@end
+@protocol JLMenuDelegate <NSObject>
+@optional
+- (void)menuView:(JLMenuView *)menuView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)didCloseMenu:(JLMenuView *)menuView;
+@end
+
+
+
+@interface JLMenuView : UIView
+
+
+@property (nonatomic, weak) id<JLMenuDelegate> delegate;
+@property (nonatomic, weak) id<JLMenuDataSource> dataSource;
+- (instancetype)initWithPoint:(CGPoint)point inView:(UIView *)fromeView ;
+- (void)show;
+@end
+
---
+++ JLKitDemo/JLKitDemo/JLMenuView.m
@@ -0,0 +1,418 @@
+//
+//  JLMEenu.m
+//  JLMenuDemo
+//
+//  Created by 王建龙 on 15/11/28.
+//  Copyright © 2015年 AngelLL. All rights reserved.
+//
+
+#import "JLMenuView.h"
+
+#define Margin 10.0f
+#define CellLineEdgeInsets UIEdgeInsetsMake(0, 10, 0, 10)
+#define kScreenWidth        [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight        [UIScreen mainScreen].bounds.size.height
+
+#pragma mark - 类:JLView -
+@interface JLView : UIView
+
+@property (nonatomic, strong) UIColor *pointFillColor;
+/**箭头样式*/
+@property (nonatomic, assign) PointDirection pointStyle;
+/**箭头出现的边*/
+@property (nonatomic, assign) PointAppearDirection pointAppearDirection;
+@end
+
+
+@implementation JLView
+
+- (void)drawRect:(CGRect)rect
+
+{
+//    [super drawRect:rect];
+    CGFloat x = 0.0 ;//x为起始点的坐标x
+    CGFloat y = 0.0;//y为起始点的坐标y
+    CGFloat w = rect.size.width;
+    CGFloat h = rect.size.height;
+    CGFloat locationX = w/6;
+    if (self.pointStyle == PointDirectionLeft) {
+        x = rect.origin.x + locationX;
+        y = rect.origin.y + Margin;
+    }else if (self.pointStyle == PointDirectionMiddle) {
+        x = rect.origin.x + w/2 - Margin;
+        y = rect.origin.y + Margin;
+    }else {
+        x = rect.origin.x + w -2*Margin-locationX;
+        y = rect.origin.y + Margin;
+    }
+    
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+
+    if (self.pointAppearDirection == PointAppearDirectionTop) {
+        CGContextMoveToPoint(context,x, y);//设置起点
+        
+        CGContextAddLineToPoint(context,x+Margin, y-Margin);//三角顶点
+        
+        CGContextAddLineToPoint(context,x+2*Margin, y);//终点
+        CGContextSetLineWidth(context, 0);
+    }else if (self.pointAppearDirection == PointAppearDirectionBottom) {
+        
+            CGContextMoveToPoint(context,x, h - Margin);//设置起点
+        
+            CGContextAddLineToPoint(context,x+Margin, h);//三角顶点
+        
+            CGContextAddLineToPoint(context,x+2*Margin, h - Margin);//终点
+            CGContextSetLineWidth(context, 0);
+    }else{
+        
+    }
+    
+    
+    
+    
+    
+    
+    CGContextClosePath(context);//路径结束标志，不写默认封闭
+    [self.fillColor setFill]; //设置填充色
+    CGContextDrawPath(context,kCGPathFillStroke);//绘制路径path
+}
+
+- (void)setPointFillColor:(UIColor *)pointFillColor{
+    if (_pointFillColor != pointFillColor) {
+        _pointFillColor = pointFillColor;
+    }
+    
+}
+- (UIColor *)fillColor{
+    if (_pointFillColor) {
+        return _pointFillColor;
+    }
+    return [UIColor blackColor];
+}
+
+//- (PointDirection)pointStyle{
+//    if (_pointStyle == 1) {
+//        return PointDirectionMiddle;
+//    }
+//    return _pointStyle;
+//}
+//- (void)setPointStyle:(PointDirection)pointStyle{
+//    _pointStyle = pointStyle;
+//    [self setNeedsDisplay];
+//}
+@end
+
+#pragma mark - 类:JLMenuViewCell -
+
+@interface JLMenuViewCell : UICollectionViewCell
+/**图像*/
+@property (nonatomic, weak)UIImageView *iconView;
+/**标题*/
+@property (nonatomic, weak)UILabel *title;
+@end
+@implementation JLMenuViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self creatSubViews];
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+- (instancetype)initWithCoder:(NSCoder *)aDecoder{
+    if (self =[super initWithCoder:aDecoder]) {
+        [self creatSubViews];
+    }
+    return self;
+}
+- (void)creatSubViews{
+    UIImageView *image = [[UIImageView alloc]initWithFrame:CGRectZero];
+    [self.contentView addSubview:image];
+    image.contentMode = UIViewContentModeCenter;
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectZero];
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.contentView addSubview:label];
+    label.font = [UIFont systemFontOfSize:13];
+    label.textColor = [UIColor whiteColor];
+    
+    self.title = label;
+    self.iconView = image;
+}
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    CGFloat w = self.frame.size.width;
+    CGFloat h = self.frame.size.height;
+    self.iconView.frame =CGRectMake(0, 0, w, h*2/3);
+    self.title.frame =CGRectMake(0, CGRectGetMaxY(self.iconView.frame)-5, w, h/3);
+}
+
+@end
+#pragma mark - 类:JLMenuView -
+@interface JLMenuView ()<UITableViewDataSource, UITableViewDelegate,UICollectionViewDataSource,UICollectionViewDelegate>
+
+@property (nonatomic, weak) UITableView *tableView;
+@property (nonatomic, weak) UICollectionView *collectionView;
+@property (nonatomic, assign) CGPoint origin;
+@property (nonatomic, assign) CGFloat rowHeight;
+/**数据源*/
+@property (nonatomic, strong)NSArray *dataArray;
+/**对应显示的图片*/
+@property (nonatomic, strong)NSArray *imageArray;
+/**window载体*/
+@property (nonatomic, weak)JLView *mainView;
+/**视图类型*/
+@property (nonatomic, assign)MenuViewType viewType;
+
+/**箭头出现的位置*/
+@property (nonatomic, assign) PointAppearDirection pointAppearDirection;
+
+/**指向*/
+@property (nonatomic, assign)PointDirection pointType;
+
+/**内容颜色*/
+@property (nonatomic, strong)UIColor *contentColor;
+
+/**title颜色*/
+@property (nonatomic, strong)UIColor *titleColor;
+
+@end
+
+
+@implementation JLMenuView
+
+#pragma mark - interface -
+- (void)show{
+   UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
+    self.frame = keyWindow.bounds;
+    [keyWindow addSubview:self];
+}
+- (instancetype)initWithPoint:(CGPoint)point inView:(UIView *)fromeView {
+    self = [super init];
+    if (self) {
+        self.origin = [self convertPoint:point fromView:fromeView];
+    }
+    return self;
+}
+
+
+- (void)creatSubviewsWithFrame:(CGPoint)origin{
+    CGFloat x;
+    CGFloat y;
+    CGFloat width = [self.dataSource menuViewWidth:self];
+    CGFloat locationX = width/6;
+    CGFloat height = self.dataArray.count *44.0f;
+    if ( height>=100 || height <=0) {
+        height = 200;
+    }
+    if (self.pointType == PointDirectionLeft) {
+        x = origin.x - Margin - locationX;
+        y = origin.y;
+    }else if (self.pointType == PointDirectionMiddle){
+        x = origin.x - width/2;
+        y = origin.y;
+    }else {
+        x = origin.x - width + Margin + locationX;
+        y = origin.y;
+    }
+    
+    CGRect frame = CGRectZero;
+    CGRect backViewFram = CGRectZero;
+    switch (self.pointAppearDirection) {
+        case PointAppearDirectionTop:
+        {
+            backViewFram = CGRectMake(x, y, width, height+ Margin);
+            frame = CGRectMake(0, Margin, width, height);
+        }
+            break;
+        case PointAppearDirectionBottom:
+        {
+            backViewFram = CGRectMake(x, y-height - Margin, width, height+ Margin);
+            frame = CGRectMake(0, 0, width, height);
+        }
+            break;
+        default:
+            break;
+    }
+    
+    JLView *mainView = [[JLView alloc]initWithFrame:backViewFram];
+    mainView.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.3];
+    mainView.pointFillColor = self.contentColor;
+    mainView.pointStyle = self.pointType;
+    mainView.pointAppearDirection = self.pointAppearDirection;
+    
+    if (self.viewType == MenuViewTypeTableView) {
+        UITableView *tableView = [[UITableView alloc] initWithFrame:frame style:UITableViewStylePlain];
+        
+        tableView.backgroundColor = self.contentColor;
+        tableView.showsHorizontalScrollIndicator = NO;
+        tableView.showsVerticalScrollIndicator = NO;
+        tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        tableView.dataSource = self;
+        tableView.delegate = self;
+        [mainView addSubview:tableView];
+        tableView.layer.cornerRadius = 5;
+        tableView.bounces = NO;
+        [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"JLMenu"];
+        self.tableView = tableView;
+    }else if (self.viewType == MenuViewTypeCollectionView){
+        UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc]init];
+        layout.minimumLineSpacing = 0;
+        layout.minimumInteritemSpacing = 0;
+        layout.itemSize = CGSizeMake((width)/2, (width)/2);
+        UICollectionView * collectionView = [[UICollectionView alloc]initWithFrame:frame collectionViewLayout:layout];
+        [collectionView registerClass:[JLMenuViewCell class] forCellWithReuseIdentifier:@"jlcollectionView"];
+        collectionView.delegate = self;
+        collectionView.showsHorizontalScrollIndicator = NO;
+        collectionView.showsVerticalScrollIndicator = NO;
+        collectionView.backgroundColor = self.contentColor;
+        collectionView.dataSource = self;
+        collectionView.layer.cornerRadius = 5;
+        [mainView addSubview:collectionView];
+        self.collectionView = collectionView;
+    }
+    [self addSubview:mainView];
+    self.mainView = mainView;
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.dataArray.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"JLMenu"];
+    cell.backgroundColor = [UIColor clearColor];
+    cell.textLabel.textColor = self.titleColor;
+    cell.textLabel.font = [UIFont systemFontOfSize:15];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    //设置cell点击不变色
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    if (self.imageArray.count > indexPath.row && self.imageArray[indexPath.row] != nil) {
+        cell.imageView.image = self.imageArray[indexPath.row];
+        
+    }
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if([self.delegate respondsToSelector:@selector(menuView:didSelectRowAtIndexPath:)]){
+        [self.delegate menuView:self didSelectRowAtIndexPath:indexPath];
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self dismissWithCompletion];
+}
+#pragma mark - lazy -
+- (NSArray *)imageArray{
+    if (_imageArray == nil) {
+        NSMutableArray *array =[NSMutableArray arrayWithCapacity:[self.dataSource numberOfSubmenusInCustomMenu:self]];
+        if ([self.dataSource respondsToSelector:@selector(imageForSubmenuInCustomMenu:)]) {
+            [array addObjectsFromArray:[self.dataSource imageForSubmenuInCustomMenu:self]];
+        }
+        
+        _imageArray = [array copy];
+    }
+    return _imageArray;
+}
+- (NSArray *)dataArray{
+    if (_dataArray == nil) {
+        
+        NSMutableArray *array =[NSMutableArray arrayWithCapacity:[self.dataSource numberOfSubmenusInCustomMenu:self]];
+        [array addObjectsFromArray:[self.dataSource titleForSubmenuInCustomMenu:self]];
+        _dataArray = [array copy];
+    }
+    return _dataArray;
+}
+- (UIColor *)contentColor{
+    if ([self.dataSource respondsToSelector:@selector(menuViewContentColor:)]) {
+        return [self.dataSource menuViewContentColor:self];
+    }
+    return [UIColor blackColor];
+}
+- (UIColor *)titleColor{
+    if ([self.dataSource respondsToSelector:@selector(menuViewTitleColor:)]) {
+        return [self.dataSource menuViewContentColor:self];
+    }
+    return [UIColor whiteColor];
+}
+- (void)setDataSource:(id<JLMenuDataSource>)dataSource{
+    if (_dataSource != dataSource) {
+        _dataSource = dataSource;
+        [self creatSubviewsWithFrame:self.origin];
+    }
+}
+
+- (MenuViewType)viewType{
+    if ([self.dataSource respondsToSelector:@selector(menuViewType:)]) {
+        return [self.dataSource menuViewType:self];
+    }
+    return MenuViewTypeTableView;
+}
+- (PointAppearDirection)pointAppearDirection{
+    if ([self.dataSource respondsToSelector:@selector(menuViewPointAppearanceDirection:)]) {
+        return [self.dataSource menuViewPointAppearanceDirection:self];
+    }
+    return PointAppearDirectionTop;
+}
+- (PointDirection)pointType{
+    if ([self.dataSource respondsToSelector:@selector(menuViewDirection:)]) {
+        return [self.dataSource menuViewDirection:self];
+    }
+    return PointDirectionMiddle;
+}
+#pragma mark - dismiss -
+- (void)dismissWithCompletion{
+    if (self.superview) {
+        
+        [self removeFromSuperview];
+        if ([self.delegate  respondsToSelector:@selector(didCloseMenu:)]) {
+            [self.delegate didCloseMenu:self];
+        }
+    }
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self dismissWithCompletion];
+}
+#pragma mark - UICollectionView -
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView{
+    return 1;
+}
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return self.dataArray.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    JLMenuViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"jlcollectionView" forIndexPath:indexPath];
+    NSLog(@"%lu",indexPath.row);
+    if (self.imageArray.count > indexPath.row && self.imageArray.count <= self.dataArray.count) {
+        if (self.imageArray[indexPath.row]) {
+            [cell.iconView setImage:self.imageArray[indexPath.row]];
+        }
+    }
+    
+    if (self.dataArray[indexPath.row]) {
+        [cell.title setText:self.dataArray[indexPath.row]];
+    }
+    return cell;
+    
+}
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath{
+    if ([self.delegate respondsToSelector:@selector(menuView:didSelectRowAtIndexPath:)]) {
+        [self.delegate menuView:self didSelectRowAtIndexPath:indexPath];
+    }
+    [collectionView deselectItemAtIndexPath:indexPath animated:YES];
+    [self dismissWithCompletion];
+}
+@end
+
+
