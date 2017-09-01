28a6cf548fa3169260ead28fa9e5525e482441c3
wangjianlong <wangjianlong@xiaomi.com>
2017-09-01 19:12:03 +0800

    修改接口

--- JLKitDemo/JLKitDemo/bothsides btn/MultifunctionBtn.h
+++ JLKitDemo/JLKitDemo/bothsides btn/MultifunctionBtn.h
@@ -2,24 +2,24 @@
 //  Multifunction.h
 //  MiTalk
 //
-//  Created by mi on 2017/9/1.
+//  Created by wangjianlong on 2017/9/1.
 //  Copyright © 2017年 Xiaomi. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSInteger, MTFunctionBtnStyle) {
-    MTFunctionBtnStyleTitleRight = 0,
-    MTFunctionBtnStyleTitleLeft,
-    MTFunctionBtnStyleTitleTop,
-    MTFunctionBtnStyleTitleBottom
+typedef NS_ENUM(NSInteger, MultifunctionBtnStyle) {
+    MultifunctionBtnStyleTitleRight = 0,
+    MultifunctionBtnStyleTitleLeft,
+    MultifunctionBtnStyleTitleTop,
+    MultifunctionBtnStyleTitleBottom
     
 };
 
 @interface MultifunctionBtn : UIButton
 
 
-@property (nonatomic, assign)  MTFunctionBtnStyle btnStyle;
+@property (nonatomic, assign)  MultifunctionBtnStyle btnStyle;
 
 /**副标题*/
 @property (nonatomic, weak) UILabel *subTitleLabel;
--- JLKitDemo/JLKitDemo/bothsides btn/MultifunctionBtn.m
+++ JLKitDemo/JLKitDemo/bothsides btn/MultifunctionBtn.m
@@ -2,7 +2,7 @@
 //  Multifunction.m
 //  MiTalk
 //
-//  Created by mi on 2017/9/1.
+//  Created by wangjianlong on 2017/9/1.
 //  Copyright © 2017年 Xiaomi. All rights reserved.
 //
 
@@ -31,11 +31,11 @@
     CGFloat w = CGRectGetWidth(contentRect);
     CGFloat h = CGRectGetHeight(contentRect);
     
-    if (self.btnStyle == MTFunctionBtnStyleTitleLeft) {
+    if (self.btnStyle == MultifunctionBtnStyleTitleLeft) {
         return [super imageRectForContentRect:contentRect];
-    }else if (self.btnStyle == MTFunctionBtnStyleTitleTop){
+    }else if (self.btnStyle == MultifunctionBtnStyleTitleTop){
         return CGRectMake(0, 0, w, h/2);
-    }else if (self.btnStyle == MTFunctionBtnStyleTitleBottom){
+    }else if (self.btnStyle == MultifunctionBtnStyleTitleBottom){
         return CGRectMake(0, h/2, w, h/2);
     }else{
         return [super titleRectForContentRect:contentRect];
@@ -49,11 +49,11 @@
     CGFloat h = CGRectGetHeight(contentRect);
     
     
-    if (self.btnStyle == MTFunctionBtnStyleTitleLeft) {
+    if (self.btnStyle == MultifunctionBtnStyleTitleLeft) {
         return [super titleRectForContentRect:contentRect];
-    }else if (self.btnStyle == MTFunctionBtnStyleTitleTop){
+    }else if (self.btnStyle == MultifunctionBtnStyleTitleTop){
         return CGRectMake(0, h/2, w, h/2);
-    }else if (self.btnStyle == MTFunctionBtnStyleTitleBottom){
+    }else if (self.btnStyle == MultifunctionBtnStyleTitleBottom){
         return CGRectMake(0, 0, w, h/2);
     }else{
         return [super imageRectForContentRect:contentRect];
